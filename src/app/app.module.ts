d11961669adaaa04910ff3813c3927ae709b8dfa
Sarah <sarahmenezesbraga@hotmail.com>
2021-07-29 17:16:57 -0300

    criação page cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,51 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { ComponentsModule } from './components/components.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { LayoutModule } from '@angular/cdk/layout';
 import { ListagemComponent } from './pages/listagem/listagem.component';
 import { DetalhesComponent } from './pages/detalhes/detalhes.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ListagemComponent,
-    DetalhesComponent
+    DetalhesComponent,
+    CadastroComponent,
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     LayoutModule,
     ComponentsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue : 'en-GB'}
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -0,0 +1,13 @@
+import { getLocaleDateFormat } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.scss'],
+})
+export class CadastroComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
