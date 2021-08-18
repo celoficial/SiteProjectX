dc02be28993be06abeeebdb299c155e41b933ae0
Marcelo Rocha <celoficial@hotmail.com>
2021-08-18 17:17:43 -0300

    .

--- src/app/pages/listagem/listagem.component.ts
+++ src/app/pages/listagem/listagem.component.ts
@@ -1,67 +1,73 @@
 import { CardModel } from '../../models/cardModel';
 import { Component, OnInit } from '@angular/core';
+import {} from './listagem.component.spec';
 
 @Component({
   selector: 'app-listagem',
   templateUrl: './listagem.component.html',
-  styleUrls: ['./listagem.component.scss']
+  styleUrls: ['./listagem.component.scss'],
 })
 export class ListagemComponent implements OnInit {
-
   listCards: Array<CardModel> = [
     {
-      title: "Título",
-      id: "hahaha",
-      thumburl: "https://alternativanerd.com.br/wp-content/uploads/2020/03/Kaguya1.jpg",
+      title: 'Título',
+      id: 'hahaha',
+      thumburl:
+        'https://alternativanerd.com.br/wp-content/uploads/2020/03/Kaguya1.jpg',
       rating: 1.5,
     },
     {
-      title: "Título1",
-      id: "sdfsdfsdf",
-      thumburl: "https://alternativanerd.com.br/wp-content/uploads/2020/03/Kaguya1.jpg",
+      title: 'Título1',
+      id: 'sdfsdfsdf',
+      thumburl:
+        'https://alternativanerd.com.br/wp-content/uploads/2020/03/Kaguya1.jpg',
       rating: 3.5,
     },
     {
-      title: "Título2",
-      id: "hfghfgh",
-      thumburl: "https://alternativanerd.com.br/wp-content/uploads/2020/03/Kaguya1.jpg",
+      title: 'Título2',
+      id: 'hfghfgh',
+      thumburl:
+        'https://alternativanerd.com.br/wp-content/uploads/2020/03/Kaguya1.jpg',
       rating: 5,
     },
     {
-      title: "Título",
-      id: "hahaha",
-      thumburl: "https://alternativanerd.com.br/wp-content/uploads/2020/03/Kaguya1.jpg",
+      title: 'Título',
+      id: 'hahaha',
+      thumburl:
+        'https://alternativanerd.com.br/wp-content/uploads/2020/03/Kaguya1.jpg',
       rating: 4,
     },
     {
-      title: "Título",
-      id: "hahaha",
-      thumburl: "https://alternativanerd.com.br/wp-content/uploads/2020/03/Kaguya1.jpg",
+      title: 'Título',
+      id: 'hahaha',
+      thumburl:
+        'https://alternativanerd.com.br/wp-content/uploads/2020/03/Kaguya1.jpg',
       rating: 3.8,
     },
     {
-      title: "Título",
-      id: "hahaha",
-      thumburl: "https://alternativanerd.com.br/wp-content/uploads/2020/03/Kaguya1.jpg",
+      title: 'Título',
+      id: 'hahaha',
+      thumburl:
+        'https://alternativanerd.com.br/wp-content/uploads/2020/03/Kaguya1.jpg',
       rating: 2.4,
     },
     {
-      title: "Título",
-      id: "hahaha",
-      thumburl: "https://alternativanerd.com.br/wp-content/uploads/2020/03/Kaguya1.jpg",
+      title: 'Título',
+      id: 'hahaha',
+      thumburl:
+        'https://alternativanerd.com.br/wp-content/uploads/2020/03/Kaguya1.jpg',
       rating: 2.5,
     },
     {
-      title: "Título",
-      id: "hahaha",
-      thumburl: "https://alternativanerd.com.br/wp-content/uploads/2020/03/Kaguya1.jpg",
+      title: 'Título',
+      id: 'hahaha',
+      thumburl:
+        'https://alternativanerd.com.br/wp-content/uploads/2020/03/Kaguya1.jpg',
       rating: 2.5,
     },
-  ]
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
