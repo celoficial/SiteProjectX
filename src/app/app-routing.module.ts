b757d9e759e1fac5daee0087a2fe9fd7f8418525
Sarah <sarahmenezesbraga@hotmail.com>
2021-08-26 04:00:01 -0300

    Criação pagina 404, Criação pagina Busca
    Alteração nome component de "search" para "form"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,38 +5,45 @@ import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { DetalhesComponent } from './pages/detalhes/detalhes.component';
 import { ListagemComponent } from './pages/listagem/listagem.component';
 import { LoginComponent } from './pages/login/login.component';
+import { SearchComponent } from './pages/search/search.component';
+import { WrongRouteComponent } from './pages/wrong-route/wrong-route.component';
 
-  const routes: Routes = [
-    {
-      path:"",
-      component:ListagemComponent
-    },
-    {
-      path:"details",
-      component: DetalhesComponent
-    },
-    {
-      path: "cadastro",
-      component: CadastroComponent
-    },
-
-    {path: "minhalista",
-    component: MinhalistaComponent
+const routes: Routes = [
+  {
+    path: '',
+    component: ListagemComponent,
   },
   {
-    path: "login",
-    component: LoginComponent
+    path: 'details',
+    component: DetalhesComponent,
+  },
+  {
+    path: 'cadastro',
+    component: CadastroComponent,
   },
 
-
+  { path: 'minhalista', component: MinhalistaComponent },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'search',
+    component: SearchComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'NotFound',
+    pathMatch: 'full',
+  },
+  {
+    path: 'NotFound',
+    component: WrongRouteComponent,
+  },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
-  exports: [
-    RouterModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
